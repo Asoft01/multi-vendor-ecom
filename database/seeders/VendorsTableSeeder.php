2a12b9c7827b2b949f5e350ade596adefcdcbacd
Adeleke Hammed Lekan <lekhad19@gmail.com>
2022-03-21 08:38:29 -0400

    Added VendorsTableSeeder

---
+++ database/seeders/VendorsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Vendor;
+
+class VendorsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $vendorRecords = [
+            ['id' => 1, 'name' => 'John', 'address' => 'CP-112']
+        ];
+    }
+}
