f39dec59eab38252bb96cc416876ac72067042dc
Adeleke Hammed Lekan <lekhad19@gmail.com>
2022-04-26 12:58:54 -0400

    Make Multi Vendor E-commerce Website _ Categories Module (I) _ Create Table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
         // $this->call(VendorsTableSeeder::class);
         // $this->call(VendorsBusinessDetailsTableSeeder::class);
         // $this->call(VendorsBankDetailsTableSeeder::class);
-        $this->call(SectionsTableSeeder::class);
+        // $this->call(SectionsTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
         // $this->call(AdminsTableSeeder::class);
     }
 }
