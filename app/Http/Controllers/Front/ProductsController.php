f714efad202b721bc5711d4ff6186d23e0fa8ddb
Adeleke Hammed <lekhad19@gmail.com>
2022-08-01 16:01:24 +0100

    Products Controller Listings

--- app/Http/Controllers/Front/ProductsController.php
+++ app/Http/Controllers/Front/ProductsController.php
@@ -14,42 +14,117 @@ class ProductsController extends Controller
         if($request->ajax()){
             $data = $request->all();
             echo "<pre>"; print_r($data); die;
-        }
-        // echo "test"; die;
-        // echo $url = Route::getFacadeRoot()->current()->uri(); die;
-        $url = Route::getFacadeRoot()->current()->uri();
-        $categoryCount = Category::where(['url' => $url, 'status' => 1])->count();
-        if($categoryCount > 0){
-            // Get Category Details 
-            $categoryDetails = Category::categoryDetails($url);
-            // dd($categoryDetails); die;
-            // $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->get()->toArray();
-            $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1);
+              // echo "test"; die;
+                // echo $url = Route::getFacadeRoot()->current()->uri(); die;
+                // $url = Route::getFacadeRoot()->current()->uri();
+                $url = $data['url'];
+                $_GET['sort'] = $data['sort'];
+                $categoryCount = Category::where(['url' => $url, 'status' => 1])->count();
+                if($categoryCount > 0){
+                    // Get Category Details 
+                    $categoryDetails = Category::categoryDetails($url);
+                    // dd($categoryDetails); die;
+                    // $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->get()->toArray();
+                    $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1);
 
-            // checking for sort s
-            if(isset($_GET['sort']) && !empty($_GET['sort'])){
-                if($_GET['sort'] == "product_latest"){
-                    $categoryProducts->orderBy('products.id', 'Desc');
-                }else if($_GET['sort'] == "price_lowest"){
-                    $categoryProducts->orderBy('products.product_price', 'Asc');
-                }else if($_GET['sort'] == "price_highest"){
-                    $categoryProducts->orderBy('products.product_price', 'Desc');
-                }else if($_GET['sort'] == "name_z_a"){
-                    $categoryProducts->orderBy('products.product_name', 'Desc');
-                }else if($_GET['sort'] == "name_a_z"){
-                    $categoryProducts->orderBy('products.product_name', 'Asc');
+                    // checking for sort s
+                    if(isset($_GET['sort']) && !empty($_GET['sort'])){
+                        if($_GET['sort'] == "product_latest"){
+                            $categoryProducts->orderBy('products.id', 'Desc');
+                        }else if($_GET['sort'] == "price_lowest"){
+                            $categoryProducts->orderBy('products.product_price', 'Asc');
+                        }else if($_GET['sort'] == "price_highest"){
+                            $categoryProducts->orderBy('products.product_price', 'Desc');
+                        }else if($_GET['sort'] == "name_z_a"){
+                            $categoryProducts->orderBy('products.product_name', 'Desc');
+                        }else if($_GET['sort'] == "name_a_z"){
+                            $categoryProducts->orderBy('products.product_name', 'Asc');
+                        }
+                    }
+
+                    $categoryProducts = $categoryProducts->Paginate(3);
+                    // $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->simplePaginate(3);
+                    // $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->cursorPaginate(3);
+                    // dd($categoryProducts); die;
+                    // dd($categoryDetails); die;
+                    // echo "Category Exists"; die;
+                    return view('front.products.listing')->with(compact('categoryDetails', 'categoryProducts', 'url'));
+                }else{
+                    abort(404);
                 }
-            }
-
-            $categoryProducts = $categoryProducts->Paginate(3);
-            // $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->simplePaginate(3);
-            // $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->cursorPaginate(3);
-            // dd($categoryProducts); die;
-            // dd($categoryDetails); die;
-            // echo "Category Exists"; die;
-            return view('front.products.listing')->with(compact('categoryDetails', 'categoryProducts', 'url'));
         }else{
-            abort(404);
+            // echo "test"; die;
+            // echo $url = Route::getFacadeRoot()->current()->uri(); die;
+            $url = Route::getFacadeRoot()->current()->uri();
+            $categoryCount = Category::where(['url' => $url, 'status' => 1])->count();
+            if($categoryCount > 0){
+                // Get Category Details 
+                $categoryDetails = Category::categoryDetails($url);
+                // dd($categoryDetails); die;
+                // $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->get()->toArray();
+                $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1);
+
+                // checking for sort s
+                if(isset($_GET['sort']) && !empty($_GET['sort'])){
+                    if($_GET['sort'] == "product_latest"){
+                        $categoryProducts->orderBy('products.id', 'Desc');
+                    }else if($_GET['sort'] == "price_lowest"){
+                        $categoryProducts->orderBy('products.product_price', 'Asc');
+                    }else if($_GET['sort'] == "price_highest"){
+                        $categoryProducts->orderBy('products.product_price', 'Desc');
+                    }else if($_GET['sort'] == "name_z_a"){
+                        $categoryProducts->orderBy('products.product_name', 'Desc');
+                    }else if($_GET['sort'] == "name_a_z"){
+                        $categoryProducts->orderBy('products.product_name', 'Asc');
+                    }
+                }
+
+                $categoryProducts = $categoryProducts->Paginate(3);
+                // $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->simplePaginate(3);
+                // $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->cursorPaginate(3);
+                // dd($categoryProducts); die;
+                // dd($categoryDetails); die;
+                // echo "Category Exists"; die;
+                return view('front.products.listing')->with(compact('categoryDetails', 'categoryProducts', 'url'));
+            }else{
+                abort(404);
+            }
         }
+        // // echo "test"; die;
+        // // echo $url = Route::getFacadeRoot()->current()->uri(); die;
+        // $url = Route::getFacadeRoot()->current()->uri();
+        // $categoryCount = Category::where(['url' => $url, 'status' => 1])->count();
+        // if($categoryCount > 0){
+        //     // Get Category Details 
+        //     $categoryDetails = Category::categoryDetails($url);
+        //     // dd($categoryDetails); die;
+        //     // $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->get()->toArray();
+        //     $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1);
+
+        //     // checking for sort s
+        //     if(isset($_GET['sort']) && !empty($_GET['sort'])){
+        //         if($_GET['sort'] == "product_latest"){
+        //             $categoryProducts->orderBy('products.id', 'Desc');
+        //         }else if($_GET['sort'] == "price_lowest"){
+        //             $categoryProducts->orderBy('products.product_price', 'Asc');
+        //         }else if($_GET['sort'] == "price_highest"){
+        //             $categoryProducts->orderBy('products.product_price', 'Desc');
+        //         }else if($_GET['sort'] == "name_z_a"){
+        //             $categoryProducts->orderBy('products.product_name', 'Desc');
+        //         }else if($_GET['sort'] == "name_a_z"){
+        //             $categoryProducts->orderBy('products.product_name', 'Asc');
+        //         }
+        //     }
+
+        //     $categoryProducts = $categoryProducts->Paginate(3);
+        //     // $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->simplePaginate(3);
+        //     // $categoryProducts = Product::with('brand')->whereIn('category_id', $categoryDetails['catIds'])->where('status', 1)->cursorPaginate(3);
+        //     // dd($categoryProducts); die;
+        //     // dd($categoryDetails); die;
+        //     // echo "Category Exists"; die;
+        //     return view('front.products.listing')->with(compact('categoryDetails', 'categoryProducts', 'url'));
+        // }else{
+        //     abort(404);
+        // }
     }
 }
