246b3f060b4cc2ff65f43686a4496732d1e24a79
Adeleke Hammed <lekhad19@gmail.com>
2022-10-22 18:47:57 -0400

    Minor fix

--- database/migrations/2022_10_18_234326_create_carts_table.php
+++ database/migrations/2022_10_18_234326_create_carts_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->integer('user_id')->nullable(); 
             $table->integer('product_id')->nullable();
             $table->string('size')->nullable();  
-            $table->integer('quantity')->nullable();  
+            $table->integer('quantity')->nullable(); 
             $table->timestamps();
         });
     }
