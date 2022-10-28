80a47e180fdb70a2f7fbeccad2fffee8dab3f5a5
Adeleke Hammed <lekhad19@gmail.com>
2022-10-28 04:50:50 -0400

    Fixed the carts page

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -14,10 +14,14 @@ class Cart extends Model
     public static function getCartItems(){
         if(Auth::check()){
             // If user logged in / pick auth id of the user 
-            $getCartItems = Cart::with('product')->orderby('id', 'Desc')->where('user_id', Auth::user()->id)->get()->toArray(); 
+            $getCartItems = Cart::with(['product' => function($query){
+                $query->select('id', 'category_id', 'product_name', 'product_code', 'product_color', 'product_image');
+            }])->orderby('id', 'Desc')->where('user_id', Auth::user()->id)->get()->toArray(); 
         }else{
             // If user not logged in / pick session id of the user 
-            $getCartItems = Cart::with('product')->orderby('id', 'Desc')->where('session_id', Session::get('session_id'))->get()->toArray();
+            $getCartItems = Cart::with(['product' => function($query){
+                $query->select('id', 'category_id', 'product_name', 'product_code', 'product_color', 'product_image');
+            }])->orderby('id', 'Desc')->where('session_id', Session::get('session_id'))->get()->toArray();
         }
         return $getCartItems; 
     }
