b9acb7c46f6054cee2ef6242924d117694026eb7
Adeleke Hammed <lekhad19@gmail.com>
2023-02-04 11:24:39 -0500

    Displaying Product Code

--- resources/views/front/orders/order_details.blade.php
+++ resources/views/front/orders/order_details.blade.php
@@ -26,6 +26,32 @@
                 <table class="table table-striped table-borderless">
                     <tr><td colspan="2"><strong>Order Details</strong></td></tr>
                     <tr><td>Order Date</td><td>{{ date('Y-m-d h:i:s', strtotime($orderDetails['created_at'])); }}</td></tr>
+                    <tr><td>Order Status</td>{{ $orderDetails['order_status'] }}<td></td></tr>
+                    <tr><td>Order Total</td><td>{{ $orderDetails['order_status'] }}</td></tr>
+                    <tr><td>Shipping Charges</td>{{ $orderDetails['shipping_charges'] }}<td></td></tr>
+                    @if($orderDetails['coupon_code'] !="")
+                        <tr><td>Coupon Code</td><td>{{ $orderDetails['coupon_code'] }}</td></tr>
+                        <tr><td>Coupon Amount</td><td>{{ $orderDetails['coupon_amount'] }}</td></tr>
+                    @endif
+                    <tr><td>Payment Method</td><td>{{ $orderDetails['payment_method'] }}</td></tr>
+                </table>
+                <table class="table table-striped table-borderless">
+                    <tr>
+                        <th>Product Code</th>
+                        <th>Product Name</th>
+                        <th>Product Size</th>
+                        <th>Product Color</th>
+                        <th>Product Qty</th>
+                    </tr>
+                    @foreach ($orderDetails['orders_products'] as $product)
+                        <tr>
+                            <td>{{ $product['product_code'] }}</td>
+                            <td>{{ $product['product_name'] }}</td>
+                            <td>{{ $product['product_size'] }}</td>
+                            <td>{{ $product['product_color'] }}</td>
+                            <td>{{ $product['product_qty'] }}</td>
+                        </tr>
+                    @endforeach
                 </table>
             </div>
         </div>
