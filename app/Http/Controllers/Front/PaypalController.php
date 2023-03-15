7df1857c51966ffe1a06926ea86fa1ebafebf315
Adeleke Hammed <lekhad19@gmail.com>
2023-03-15 18:41:01 -0400

    OmniPay

--- app/Http/Controllers/Front/PaypalController.php
+++ app/Http/Controllers/Front/PaypalController.php
@@ -5,14 +5,38 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Session;
+use Ominpay\Omnipay;
 
 class PaypalController extends Controller
 {
+    private $gateway;
+
+    public function __construct(){
+        $this->gateway = Omnipay::create('Paypal_Rest');
+        $this->gateway->setClientId(env('PAYPAL_CLIENT_ID'));
+        $this->gateway->setSecret(env('PAYPAL_CLIENT_SECRET'));
+        $this->gateway->setTestMode(true);
+    }
+
     public function paypal(){
         if(Session::has('order_id')){
             return view('front.paypal.paypal');
         }else{
             return redirect('cart');
+        }
+    }
+
+    public function pay(Request $request){
+        try{
+            $paypal_amount = round(Session::get('grand_total')/80,2);
+            $response = $this->gateway->purchase(array(
+                'amount' => $paypal_amount, 
+                'currency' => env('PAYPAL_CURRENCY'), 
+                'returnUrl' => url('success'), 
+                'cancelUrl' => url('error')
+            ))->send();
+        }catch (\Throwable $th){
+            return $th->getMessage();
         }
     }
 }
