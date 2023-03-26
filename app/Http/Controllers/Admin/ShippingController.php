25ef4a9585d4e6a651df16c890216e14aa116182
Adeleke Hammed <lekhad19@gmail.com>
2023-03-26 11:11:47 -0400

    Shipping

--- app/Http/Controllers/Admin/ShippingController.php
+++ app/Http/Controllers/Admin/ShippingController.php
@@ -12,5 +12,7 @@ class ShippingController extends Controller
     public function shippingCharges(){
         Session::put('page', 'shipping'); 
         $shippinCharges = ShippingCharge::get()->toArray(); 
+        // dd($shippinCharges); die;
+        return view('admin.shipping.shipping_charges')->with(compact('shippingCharges')); 
     }
 }
