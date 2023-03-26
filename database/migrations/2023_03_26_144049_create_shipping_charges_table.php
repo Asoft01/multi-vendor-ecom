5b0ab5d73f7c32ec64bb871706017622114595e1
Adeleke Hammed <lekhad19@gmail.com>
2023-03-26 10:44:50 -0400

    Shipping Charges

---
+++ database/migrations/2023_03_26_144049_create_shipping_charges_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipping_charges', function (Blueprint $table) {
+            $table->id();
+            $table->string('country')->nullable();
+            $table->float('rate')->nullable();
+            $table->tinyInteger('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipping_charges');
+    }
+};
