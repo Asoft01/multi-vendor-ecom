905c1db994bc3bfd2a9c5bda3a4b45953dc96834
Adeleke Hammed <lekhad19@gmail.com>
2023-04-25 17:58:03 +0100

    Get Shipping Charges

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -15,12 +15,12 @@ class Cart extends Model
         if(Auth::check()){
             // If user logged in / pick auth id of the user 
             $getCartItems = Cart::with(['product' => function($query){
-                $query->select('id', 'category_id', 'product_name', 'product_code', 'product_color', 'product_image');
+                $query->select('id', 'category_id', 'product_name', 'product_code', 'product_color', 'product_image', 'product_weight');
             }])->orderby('id', 'Desc')->where('user_id', Auth::user()->id)->get()->toArray(); 
         }else{
             // If user not logged in / pick session id of the user 
             $getCartItems = Cart::with(['product' => function($query){
-                $query->select('id', 'category_id', 'product_name', 'product_code', 'product_color', 'product_image');
+                $query->select('id', 'category_id', 'product_name', 'product_code', 'product_color', 'product_image', 'product_weight');
             }])->orderby('id', 'Desc')->where('session_id', Session::get('session_id'))->get()->toArray();
         }
         return $getCartItems; 
--- app/Models/ShippingCharge.php
+++ app/Models/ShippingCharge.php
@@ -9,9 +9,27 @@ class ShippingCharge extends Model
 {
     use HasFactory;
 
-    public static function getShippingCharges($country){
-        $getShippingCharges = ShippingCharge::select('rate')->where('country', $country)->first(); 
-        return $getShippingCharges->rate; 
+    public static function getShippingCharges($total_weight, $country){
+        $shippingDetails = ShippingCharge::where('country', $country)->first()->toArray();
+        if($total_weight > 0){
+            if($total_weight > 0 && $total_weight <= 500){
+                $rate = $shippingDetails['0_500g'];
+            }else if($total_weight > 500 && $total_weight <= 1000){
+                $rate = $shippingDetails['0_500g'];
+            }else if($total_weight > 1000 && $total_weight <= 2000){
+                $rate = $shippingDetails['0_500g'];
+            }else if($total_weight > 2000 && $total_weight <= 5000){
+                $rate = $shippingDetails['0_500g'];
+            }else if($total_weight > 5000){
+                $rate = $shippingDetails['0_500g'];
+            }else {
+                $rate = 0;
+            }
+        }else{
+            $rate = 0;
+        }
+        // return $getShippingCharges->rate; 
+        return $rate;
     }
 }
 
